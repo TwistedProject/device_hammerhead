222681147ab708504cd4de03b098a01c303d25cd
aow1980 <wilkes.allen@gmail.com>
2016-04-07 17:42:27 -0400

    hammerhead: build kernel inline
    Change-Id: I03a9d9ce0cea87b5858d1a6318ab71cad45eefc2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,11 @@ BOARD_HAL_STATIC_LIBRARIES := libdumpstate.hammerhead
 
 BOARD_SEPOLICY_DIRS += device/lge/hammerhead/sepolicy
 
+# Define kernel config for inline building
+TARGET_KERNEL_CONFIG := twisted_hells_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/hells-Core-N5
+TARGET_GCC_VERSION_ARM := 4.9
+
 ifneq ($(filter hammerhead_fp aosp_hammerhead_fp,$(TARGET_PRODUCT)),)
 BOARD_SEPOLICY_DIRS += \
        device/lge/hammerhead/sepolicy-hammerhead_fp
